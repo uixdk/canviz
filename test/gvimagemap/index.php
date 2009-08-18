ab7f3833f26c3a37a34183346e0c78e6840246eb
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2009-08-18 13:20:45 +0000

    gvimagemap/index.php: Don't use base href tag because it doesn't work with Firefox here for some reason.

--- test/gvimagemap/index.php
+++ test/gvimagemap/index.php
@@ -51,23 +51,24 @@ switch ($output_format) {
 		$content_type = 'text/html; charset=' . $charset;
 		header('Content-Type: ' . $content_type);
 		
+		$base_href = dirname($_SERVER['SCRIPT_NAME']) . '/';
+		
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta name="MSSmartTagsPreventParsing" content="true" />
 	<meta http-equiv="content-type" content="<?php echo $content_type; ?>" />
 	<title><?php echo htmlspecialchars($graph_base); ?></title>
-	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']) . '/'; ?>" />
-	<link rel="stylesheet" type="text/css" href="styles.css" />
-	<script type="text/javascript" src="prototype/prototype.js"></script>
-	<script type="text/javascript" src="scripts.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo $base_href; ?>styles.css" />
+	<script type="text/javascript" src="<?php echo $base_href; ?>prototype/prototype.js"></script>
+	<script type="text/javascript" src="<?php echo $base_href; ?>scripts.js"></script>
 </head>
 <body>
 
 <div id="container">
 <img id="image" src="<?php echo htmlspecialchars($_SERVER['SCRIPT_NAME'] . '/' . $graph_base . '.png'); ?>" />
 <canvas id="canvas"></canvas>
-<img id="overlay" src="spacer.gif" usemap="#<?php echo $map_name; ?>" />
+<img id="overlay" src="<?php echo $base_href; ?>spacer.gif" usemap="#<?php echo $map_name; ?>" />
 <?php echo $cmapx; ?>
 <div id="status"></div>
 </div>
