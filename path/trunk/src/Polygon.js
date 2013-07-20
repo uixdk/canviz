010a4ae47e3c599420ad011386c0ce2d746a23eb
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2013-07-20 17:23:12 +0000

    whitespace

--- path/trunk/src/Polygon.js
+++ path/trunk/src/Polygon.js
@@ -1,8 +1,8 @@
 // Constructor
 function Polygon(points, options) {
   if (!(this instanceof Polygon)) return new Polygon(points, options);
-		this.points = points || [];
-		Path.call(this, [], options);
+  this.points = points || [];
+  Path.call(this, [], options);
 }
 
 // Parent
@@ -11,23 +11,23 @@ Polygon.prototype = new Path();
 
 // Prototype
 Polygon.prototype.constructor = Polygon;
-Polygon.prototype.offset = function(dx, dy) {
-		var pointsLength = this.points.length;
-		for (var i = 0; i < pointsLength; ++i ) {
-			this.points[i].offset(dx, dy);
-		}
-		return this;
+Polygon.prototype.offset = function (dx, dy) {
+  var pointsLength = this.points.length;
+  for (var i = 0; i < pointsLength; ++i) {
+    this.points[i].offset(dx, dy);
+  }
+  return this;
 };
-Polygon.prototype.setupSegments = function() {
-		var pointsLength = this.points.length;
-		for (var i = 0; i < pointsLength; ++i) {
-			var next = i + 1;
-			if (this.points.length == next) next = 0;
-			this.addBezier([
-				this.points[i],
-				this.points[next]
-			]);
-		}
+Polygon.prototype.setupSegments = function () {
+  var pointsLength = this.points.length;
+  for (var i = 0; i < pointsLength; ++i) {
+    var next = i + 1;
+    if (this.points.length == next) next = 0;
+    this.addBezier([
+      this.points[i],
+      this.points[next]
+    ]);
+  }
 };
 
 // Exports
