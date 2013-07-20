84c76a43ce87a6642fc18c892079d4be837cd948
ryandesign.com <ryandesign.com@96afbf3d-d051-0410-af9b-0f47d9322047>
2013-07-20 17:35:07 +0000

    whitespace

--- path/trunk/examples/example2.js
+++ path/trunk/examples/example2.js
@@ -7,47 +7,47 @@ var ctx, shapes;
 
 contentLoaded(window, function() {
   setTimeout(function() {
-	var canvas = document.getElementById('canvas');
-	if (canvas.getContext) {
-		ctx = canvas.getContext('2d');
-		ctx.lineWidth = 4;
-		
-		shapes = [
-			new Rect(-100, -100, 100, 100),
-			new Ellipse(0, 0, 100, 100, {
-				x_fill: true,
-				fillStyle: 'rgba(0, 255, 0, 0.4)',
-				x_stroke: false
-			}),
-			new Polygon([
-				new Point(-100, 100),
-				new Point(0, 100 - 100 * Math.sqrt(3)),
-				new Point(100, 100)
-			], {
-				x_fill: true,
-				fillStyle: 'rgba(0, 0, 255, 0.4)',
-				strokeStyle: 'rgba(0, 0, 255, 0.8)',
-				lineCap: 'round'
-			})
-		];
-		shapes[0].setOptions({
-			strokeStyle: 'rgba(255, 0, 0, 0.8)',
-			x_strokeType: 'dashed',
-			x_dashLength: 20
-		});
-		
-		drawFrame();
-	}
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      ctx = canvas.getContext('2d');
+      ctx.lineWidth = 4;
+
+      shapes = [
+        new Rect(-100, -100, 100, 100),
+        new Ellipse(0, 0, 100, 100, {
+          x_fill: true,
+          fillStyle: 'rgba(0, 255, 0, 0.4)',
+          x_stroke: false
+        }),
+        new Polygon([
+          new Point(-100, 100),
+          new Point(0, 100 - 100 * Math.sqrt(3)),
+          new Point(100, 100)
+        ], {
+          x_fill: true,
+          fillStyle: 'rgba(0, 0, 255, 0.4)',
+          strokeStyle: 'rgba(0, 0, 255, 0.8)',
+          lineCap: 'round'
+        })
+      ];
+      shapes[0].setOptions({
+        strokeStyle: 'rgba(255, 0, 0, 0.8)',
+        x_strokeType: 'dashed',
+        x_dashLength: 20
+      });
+
+      drawFrame();
+    }
   }, 0);
 });
 
 function drawFrame() {
-	ctx.clearRect(0, 0, 800, 400);
-	ctx.save();
-	ctx.translate(150, 150);
-	for (var i = 0; i < shapes.length; ++i) {
-		shapes[i].draw(ctx);
-		ctx.translate(250, 0);
-	}
-	ctx.restore();
+  ctx.clearRect(0, 0, 800, 400);
+  ctx.save();
+  ctx.translate(150, 150);
+  for (var i = 0; i < shapes.length; ++i) {
+    shapes[i].draw(ctx);
+    ctx.translate(250, 0);
+  }
+  ctx.restore();
 }
